c3604189de894125a11f91192787451e59ebfb04
SeanSyue <sean962081468@gmail.com>
2019-02-22 10:15:10 +0800

    init commit

---
+++ src/main/java/tf/detection/DetectionApplication.java
@@ -0,0 +1,13 @@
+package tf.detection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DetectionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DetectionApplication.class, args);
+    }
+
+}
