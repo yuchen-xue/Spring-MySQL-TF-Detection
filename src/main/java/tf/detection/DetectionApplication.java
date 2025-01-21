949565a10b85d68646fd1f07bd5204417353f6ae
SeanSyue <32350820+SeanSyue@users.noreply.github.com>
2025-01-21 15:10:30 +0000

    Remove protobuf and use a simpler label file

--- src/main/java/tf/detection/DetectionApplication.java
+++ src/main/java/tf/detection/DetectionApplication.java
@@ -1,17 +1,18 @@
 package tf.detection;
 
-import com.google.protobuf.TextFormat;
+import java.io.IOException;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.tensorflow.SavedModelBundle;
-import tf.detection.protos.StringIntLabelMapOuterClass;
-
-import java.io.IOException;
-import java.net.URL;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 
 @SpringBootApplication
 public class DetectionApplication {
@@ -38,22 +39,10 @@ public class DetectionApplication {
     }
 
     private static String[] loadLabels(String filename) throws Exception {
-        String text = Files.readString(Paths.get(filename));
-        StringIntLabelMapOuterClass.StringIntLabelMap.Builder builder = StringIntLabelMapOuterClass.StringIntLabelMap.newBuilder();
-        TextFormat.merge(text, builder);
-        StringIntLabelMapOuterClass.StringIntLabelMap proto = builder.build();
-        int maxId = 0;
-        for (StringIntLabelMapOuterClass.StringIntLabelMapItem item : proto.getItemList()) {
-            if (item.getId() > maxId) {
-                maxId = item.getId();
-            }
-        }
-        String[] ret = new String[maxId + 1];
-        for (StringIntLabelMapOuterClass.StringIntLabelMapItem item : proto.getItemList()) {
-            ret[item.getId()] = item.getDisplayName();
-        }
+        Path filePath = Paths.get(filename);
+        List<String> lines = Files.lines(filePath).collect(Collectors.toList());
+        String[] ret = lines.toArray(new String[0]);
         return ret;
     }
-
 
 }
