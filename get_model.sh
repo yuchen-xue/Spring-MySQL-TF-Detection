01825aa8fb8f1f07ba747e348a9f885294e15b6d
SeanSyue <32350820+SeanSyue@users.noreply.github.com>
2025-02-28 22:51:27 +0100

    Jump to new version and update readme

--- get_model.sh
+++
@@ -1,3 +0,0 @@
-curl -L http://download.tensorflow.org/models/object_detection/ssd_inception_v2_coco_2017_11_17.tar.gz | tar -xz
-mv ssd_inception_v2_coco_2017_11_17/saved_model ./src/main/resources/tf_inception
-rm -r ssd_inception_v2_coco_2017_11_17
